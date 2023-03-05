f2b688820b6062add8e491df3c0dbd5d0861e6c3
Rmckays <rmckays@gmail.com>
2023-03-05 15:10:35 -0700

    Add timer and improve enemy and player movement.

--- scenes/basic_enemy/basic_enemy.gd
+++ scenes/basic_enemy/basic_enemy.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-const MAX_SPEED:int = 75
+const MAX_SPEED:int = 50
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$Area2D.area_entered.connect(on_area_entered)
@@ -20,5 +20,5 @@ func get_direction_to_player():
 	return Vector2.ZERO
 
 
-func on_area_entered(otherArea: Area2D):
+func on_area_entered(other_area: Area2D):
 	queue_free()
