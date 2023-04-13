9550ccfb1103845091a4009744051c659c57e2d3
Rmckays <rmckays@gmail.com>
2023-04-13 07:36:23 -0700

    Fix upgrade manager issue.

--- scenes/managers/upgrade_manager.gd
+++ scenes/managers/upgrade_manager.gd
@@ -31,7 +31,7 @@ func apply_upgrade(upgrade: AbilityUpgrade):
 	if upgrade.max_quantity > 0:
 		var current_quantity = current_upgrades[upgrade.id]["quantity"]
 		if current_quantity == upgrade.max_quantity:
-			upgrade_pool = upgrade_pool.remove_item(upgrade)
+			upgrade_pool.remove_item(upgrade)
 	
 	update_upgrade_pool(upgrade)
 	GameEvents.emit_ability_upgrade_added(upgrade, current_upgrades)
